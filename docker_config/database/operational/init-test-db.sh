4a105e90397b4f1d2b55a3f5c4259db723bc7f5c
Jorge Galvis Cardenas <jorgegalcad@gmail.com>
2025-10-20 19:39:36 -0500

    Adjusting creation of test databases

--- docker_config/database/operational/init-test-db.sh
+++ docker_config/database/operational/init-test-db.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
+# Check if database exists, create if it doesn't
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS operational_test;
+    SELECT 'CREATE DATABASE operational_test'
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'operational_test')\gexec
     GRANT ALL PRIVILEGES ON DATABASE operational_test TO postgres;
 EOSQL
